da726db7327d036d99c9a5e2802a2b0421b384ad
YannickRobin <yannick.robin+git@gmail.com>
2022-11-08 14:53:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.get("/getEtherPrice", async(req, res, next) => {
   res.json(result);
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
  console.log("Server running on port 3000");
 });
